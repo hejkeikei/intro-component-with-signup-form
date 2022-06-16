81fe8cc9bfa04267fca7afcb33fe71893ccb7ca5
Tîng-huē Tân <71164837+hejkeikei@users.noreply.github.com>
2022-06-16 10:59:54 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var fields = document.querySelectorAll("input");
 var submitBtn = document.getElementById("submit");
-submitBtn.addEventListener("submit",function(){
+submitBtn.addEventListener("click",function(){
+    event.preventDefault();
     for(let i=0; i< fields.length-1; i++){
         if(fields[i].value==""){
             fields[i].style.border="solid 2px var(--red)";
